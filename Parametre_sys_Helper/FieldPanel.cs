4094c03335251ef6efee3f99f694e31f3517c016
YassinLokhat <yassinlokhat@gmail.com>
2019-05-16 15:00:39 +0400

    Add files via upload

---
+++ Parametre_sys_Helper/FieldPanel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parametre_sys_Helper
+{
+    public class TypePanel : GroupBox
+    {
+        public Dictionary<string, Control> Fields;
+        int margin = 5;
+
+        public TypePanel(string name, string[] fields)
+        {
+            Fields = new Dictionary<string, Control>();
+            this.Text = name;
+
+            foreach (string field in fields)
+            {
+                switch (field)
+                {
+                    case "":
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
